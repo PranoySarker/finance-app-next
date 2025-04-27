1506e228c9e4046061e805f4c904950391298bbc
PranoySarker <88031593+PranoySarker@users.noreply.github.com>
2025-04-27 23:10:37 +0600

    add transaction page and link

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -3,10 +3,16 @@ import TransactionList from "./components/TransactionList";
 import TransactionListFallback from "./components/TransactionListFallback";
 import TrendDashboad from "./components/TrendDashboad";
 import TrendFallback from "./components/TrendFallback";
+import Link from "next/link";
+import { PlusCircle } from "lucide-react";
 
 const DashBoardHomePage = () => {
   return (
     <>
+      <section className="mb-8">
+        <h1 className="text-4xl font-semibold ">Summary</h1>
+      </section>
+
       <section className="mb-8 grid grid-cols-2 lg:grid-cols-4 gap-8">
         <Suspense fallback={<TrendFallback />}>
           <TrendDashboad type="Income" />
@@ -20,6 +26,14 @@ const DashBoardHomePage = () => {
         <Suspense fallback={<TrendFallback />}>
           <TrendDashboad type="Saving" />
         </Suspense>
+      </section>
+
+      <section className="flex justify-between items-center mb-8">
+        <h2 className="text-2xl font-semibold">Transactions</h2>
+        <Link href="/dashboard/transaction/add" className={`flex items-center`}>
+          <PlusCircle className="w-4 h-4" />
+          <span className="ml-2">Add</span>
+        </Link>
       </section>
       <Suspense fallback={<TransactionListFallback />}>
         <TransactionList />
---
+++ app/dashboard/transaction/add/page.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AddTransationPage = () => {
+  return (
+    <>
+      <h2 className="text-4xl font-semibold mb-4">Add Transaction</h2>
+    </>
+  );
+};
+
+export default AddTransationPage;
